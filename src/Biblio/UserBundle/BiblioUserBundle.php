c43ffbd1a5cad2a7c13c214a792748a338f75097
Nic062 <nick62olas@free.fr>
2015-03-22 19:47:46 +0100

    1ère partie Surcharge FOS + Slider d'images

--- src/Biblio/UserBundle/BiblioUserBundle.php
+++ src/Biblio/UserBundle/BiblioUserBundle.php
@@ -6,7 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BiblioUserBundle extends Bundle
 {
-	public function getParent(){
-		return;
-	}
+	public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
